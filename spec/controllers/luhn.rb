eeee36a3b40fe6346b197c95cbb3bcc94d717e2a
Justin Cefai <cefaijustin@gmail.com>
2018-04-26 01:34:30 +0000

    factory bot added

---
+++ spec/controllers/luhn.rb
@@ -0,0 +1,6 @@
+# break the card into it's individual digits
+# start from the right and double every other number
+# if the doubled value is >= 10, take the doubled value and subtract 9 from it
+# sum the digits
+# if the sum is divisible by 10, it's a valid number
+
